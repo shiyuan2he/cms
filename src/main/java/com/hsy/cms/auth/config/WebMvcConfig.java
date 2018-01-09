7123c7a96e285e33fd2cb569cec28f796fcbd2d0
shiyuan2he <shiyuan4work@sina.com>
2018-01-09 17:52:41 +0800

    cms build

---
+++ src/main/java/com/hsy/cms/auth/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.hsy.cms.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author heshiyuan
+ * @description <p></p>
+ * @path cms/com.hsy.cms.auth.config
+ * @date 2018/1/9 17:17
+ * @github http://github.com/shiyuan2he
+ * @email shiyuan4work@sina.com
+ * Copyright (c) 2017 shiyuan4work@sina.com All rights reserved.
+ * @price ¥5    微信：hewei1109
+ */
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+}
